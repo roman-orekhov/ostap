0c423b677015df6795e0bb8b0e2aae19462a7f4d
dboulytchev <dboulytchev@gmail.com>
2009-01-12 17:46:00 +0000

    Test commit

--- sample/sample.ml
+++ sample/sample.ml
@@ -2,29 +2,18 @@ open Matcher
 open String
 open Str
 open Ostap 
+open Util
 
-let p = rule <name>=IDENT -"(" (<h>=IDENT <t>=(-"," IDENT)* {h::t}) -")" end
+let p = ostap (name:IDENT -"(" (h:IDENT t:(-"," IDENT)* {h::t}) -")")
 
-let q = rule "{" IDENT+ "}" {()} | IDENT {()} end
-let t = rule "match" q? "in" IDENT end
-
-class lexer s  =
-  let skip  = Skip.create [Skip.whitespaces " \n\t\r"] in
-  let ident = regexp "[a-zA-Z_]\([a-zA-Z_0-9]\)*" in  
-  object (self)
-      
-    inherit matcher s
-	
-    method skip p coord = skip s p coord
-    method getIDENT = self#get "identifier" ident
-	
-  end
+let q = ostap ("{" IDENT+ "}" {()} | IDENT {()})
+let t = ostap ("match" q? "in" IDENT)
 
 let parse s =
   let module P = View.NamedPair (struct let first = "name" let second = "args" end) (Token) (View.List (Token)) in
   match t (new lexer s) with
   | Parsed (x, _) -> Printf.printf "Success\n" 
-  | Failed msgs | Error msgs -> 
+  | Failed msgs -> 
       Printf.printf "Unsuccess: %s\n" 
 	(match msgs with
 	| None   -> "no description"
